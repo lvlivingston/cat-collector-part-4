ef76fb804b71bb82c4cef8ff370b13952e84a82b
lvlivingston <lvlivingston@gmail.com>
2023-12-07 09:24:42 -0600

    add view all cats page

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+# This is a baby step, normally you don't provide data in views.py... usually a model is used instead
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {
+        'cats': cats
+    })
